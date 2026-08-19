6ea2150ffcb723c21aca404f93b15e05a659d2d1
Ren <electricxangell@gmail.com>
2026-08-20 00:09:21 +0100

    pls work

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://electricxangel.github.io',
-    base: '/throwback',
+    
 });
