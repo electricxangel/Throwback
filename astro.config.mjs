7a71ed2bf89f445b949aa099cbd83280a524ae67
Ren <electricxangell@gmail.com>
2026-08-20 00:11:37 +0100

    computer generate me any web dev ability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    
-});
+export default defineConfig({});
